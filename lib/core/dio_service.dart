7f5b2ebce237f7ecd493f34f983d578837730a40
Mohamed Salah <eng.muhamed.salah@gmail.com>
2026-03-26 13:02:46 +0200

    fix login issue

--- lib/core/dio_service.dart
+++ lib/core/dio_service.dart
@@ -1,9 +1,5 @@
-
-import 'dart:convert' as convert;
-
 import 'package:ashghal/core/apiUrls.dart';
 import 'package:ashghal/core/token_storage.dart';
-import 'package:ashghal/services/Login/data/user_model.dart';
 import 'package:ashghal/services/Login/domain/register_request.dart';
 import 'package:ashghal/services/Login/domain/request.dart';
 import 'package:ashghal/services/operations/data/demands_model.dart';
@@ -22,23 +18,19 @@ class AuthService {
   Future<LoginResponse> login(LoginRequest request) async {
     print({'request': request.toJson().toString()});
     try {
-      final response = await _dio.post(
-        ApiUrls.login,
-        data: request.toJson(),
-      );
+      final response = await _dio.post(ApiUrls.login, data: request.toJson());
       print('response ${response}');
 
       if (response.data['data'] == null) {
         return LoginResponse.fromJson(response.data['message']);
       }
       return LoginResponse.fromJson(response.data['data']);
-    }
-    on DioException catch (e) {
+    } on DioException catch (e) {
       print('STATUS CODE: ${e.response?.statusCode}');
       print('ERROR BODY: ${e.response?.data}');
 
-      throw Exception(
-          e.response?.data['data'] ??  e.response?.data['message']);}
+      throw Exception(e.response?.data['data'] ?? e.response?.data['message']);
+    }
   }
 
   Future<RegisterResponse> register(RegisterRequest request) async {
@@ -52,16 +44,16 @@ class AuthService {
 
       print('from post ${response.data}');
 
-    //  return RegisterResponse.fromMap(response.data['data']);
+      //  return RegisterResponse.fromMap(response.data['data']);
       return RegisterResponse.fromMap(response.data);
     } on DioException catch (e) {
       print('STATUS CODE: ${e.response?.statusCode}');
       print('ERROR BODY: ${e.response?.data}');
 
-      throw Exception(
-          e.response?.data['data'] ??  e.response?.data['message']);
+      throw Exception(e.response?.data['data'] ?? e.response?.data['message']);
     }
   }
+
   //   print(request.toJson().toString());
   //   final response = await _dio.post(
   //     ApiUrls.register,
@@ -75,7 +67,7 @@ class AuthService {
 class DemandService {
   final TokenStorage tokenStorage;
   final Dio _dio = Dio();
-  DemandService(this.tokenStorage ){
+  DemandService(this.tokenStorage) {
     _dio.interceptors.add(
       InterceptorsWrapper(
         onRequest: (options, handler) async {
@@ -92,29 +84,25 @@ class DemandService {
   }
   Dio get dio => _dio;
 
-    // BaseOptions(
-    //   headers: {
-    //     ApiStrings.accept: ApiStrings.json,
-    //     ApiStrings.authorization : "Bearer $tokenStorage",
-    //   },
-    //   // Android emulator
-    //   // baseUrl: 'http://127.0.0.1:3000', // physical device
-    // ),
-
-
-
+  // BaseOptions(
+  //   headers: {
+  //     ApiStrings.accept: ApiStrings.json,
+  //     ApiStrings.authorization : "Bearer $tokenStorage",
+  //   },
+  //   // Android emulator
+  //   // baseUrl: 'http://127.0.0.1:3000', // physical device
+  // ),
 
   Future<List<DemandsModel>> getUserDemands() async {
     final response = await dio.get(ApiUrls.getDemands);
     print('response ${response.data['data']}');
 
     final List list = response.data['data'];
-   // print('list ${list}');
+    // print('list ${list}');
 
-    return (list)
-        .map((e) => DemandsModel.fromMap(e))
-        .toList();
+    return (list).map((e) => DemandsModel.fromMap(e)).toList();
   }
+
   //   if (response.statusCode == StatusCode.ok) {
   //
   //     //Map<String, dynamic> data = convert.jsonDecode(convert.utf8.decode(response.data)) as Map<String, dynamic>;
